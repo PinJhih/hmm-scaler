8caf0b8233459c5e01a42d0e0be11385c6f72bb2
PinJhih <isaacwang29@gmail.com>
2024-03-16 13:26:37 +0800

    Create collector/detector module

--- metric-collector/metric_collector/__init__.py
+++ metric-collector/metric_collector/__init__.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return "<h1>HMM-Scaler: API Server</h1>", 200
+    return "<h1>HMM-Scaler: Metric Collector</h1>", 200
---
+++ scaling-detector/run.py
@@ -0,0 +1,4 @@
+from scaling_detector import app
+
+if __name__ == "__main__":
+    app.run(port=7770, debug=True)
