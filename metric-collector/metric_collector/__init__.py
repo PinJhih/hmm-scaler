f25491a59fbc3c2e78f788e36feacad4e6e7d064
PinJhih <isaacwang29@gmail.com>
2024-03-16 14:40:13 +0800

    Define API of metric-collector for config update

--- metric-collector/metric_collector/__init__.py
+++ metric-collector/metric_collector/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     return "<h1>HMM-Scaler: Metric Collector</h1>", 200
+
+
+@app.route("/interval/<int:t>", methods=["PUT"])
+def set_interval(t):
+    # TODO: call method of controllers
+    msg = {"message": f"interval is set to {t}"}
+    return jsonify(msg), 200
+
+
+@app.route("/targets", methods=["PUT"])
+def set_targets():
+    # TODO: call method of controllers
+    targets = request.get_json()
+    msg = {"message": f"targets is updated"}
+    return jsonify(msg), 200
