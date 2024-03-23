e276a6bf088843a16dbbfe7da0fe031b396631bc
PinJhih <isaacwang29@gmail.com>
2024-03-23 23:14:05 +0800

    Create Web API of 'detector' to recevice states from 'collector'

--- scaling-detector/scaling_detector/__init__.py
+++ scaling-detector/scaling_detector/__init__.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     return "<h1>HMM-Scaler: Scaling Detector</h1>", 200
+
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    msg = {"message": "OK!"}
+    return jsonify(msg), 200
