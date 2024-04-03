565f210ad4b9c0f0725af17b18c28aa1b5eabc99
PinJhih <isaacwang29@gmail.com>
2024-04-03 20:06:47 +0800

    Add basic exception handling when send update to collector

--- api-server/api_server/controllers/config_mgr.py
+++ api-server/api_server/controllers/config_mgr.py
@@ -26,9 +26,8 @@ def set_interval(t):
     config.set_interval(t)
 
     # Send interval to collector
-    url = f"{__COLLECTOR_URL}/interval/{t}"
-    res = requests.put(url)
-    # TODO: Error handling
+    url = f"/interval/{t}"
+    __send_to_collector(url)
 
     msg = {"message": f"interval is set to {t}"}
     return jsonify(msg), 200
@@ -43,10 +42,8 @@ def add_target(ns, name):
     config.add_target(ns, name)
 
     # Send targets to collector
-    url = f"{__COLLECTOR_URL}/targets"
     targets = config.get_targets()
-    res = requests.put(url, json=targets)
-    # TODO: Error handling
+    __send_to_collector("/targets", targets)
 
     msg = {"message": f"{name} in {ns} is added"}
     return jsonify(msg), 200
@@ -56,9 +53,20 @@ def delete_target(ns, name):
     config.delete_target(ns, name)
 
     # Send targets to collector
-    url = f"{__COLLECTOR_URL}/targets"
     targets = config.get_targets()
-    res = requests.put(url, json=targets)
+    __send_to_collector("/targets", targets)
 
     msg = {"message": f"{name} in {ns} is deleted"}
     return jsonify(msg), 200
+
+
+def __send_to_collector(path, json=None):
+    url = __COLLECTOR_URL + path
+    try:
+        if json == None:
+            return requests.put(url)
+        else:
+            return requests.put(url, json=json)
+    except:
+        #TODO: Exception handling
+        print("[Error][API-Server] Cannot send to collector")
