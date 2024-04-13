ee9ad87725c2671f8cbad8508424facea538cba6
PinJhih <isaacwang29@gmail.com>
2024-04-13 11:56:35 +0800

    Restart worker thread when interval change

--- metric-collector/metric_collector/controllers/collector.py
+++ metric-collector/metric_collector/controllers/collector.py
@@ -20,20 +20,30 @@ class Collector:
             # TODO: Error handling
             print("[Error][Collector] Cannot get config from API-Server")
 
-    def __get_states(self):
-        print("Worker thr start")
-        while True:
-            time.sleep(self.__interval)
-            self.__states.get_all()
-            # TODO: Send states to detector
+    def __fetch_states(self, interval):
+        print(f"[Info][Collector] Worker thread (interval={interval} sec) started.")
+
+        elapsed_time = 0
+        while self.__interval == interval:
+            time.sleep(1)
+            elapsed_time += 1
+            if elapsed_time == interval and interval == self.__interval:
+                elapsed_time = 0
+                states = self.__states.get_all()
+                # TODO: Send states to detector
+        print(f"[Info][Collector] Worker thread (interval={interval} sec) killed.")
 
     def __create_thread(self):
-        thread = threading.Thread(target=self.__get_states)
+        interval = self.__interval
+        thread = threading.Thread(target=self.__fetch_states, args=[interval])
         thread.daemon = True
         return thread
 
     def set_interval(self, t):
         self.__interval = t
+        self.__worker_thread = self.__create_thread()
+        self.__worker_thread.start()
+
         msg = {"message": f"Interval is set to {t}"}
         return jsonify(msg), 200
 
